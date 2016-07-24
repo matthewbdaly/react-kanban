8b587dc9448e9cc2991738e083d02c28d7975732
Matthew Daly <matthewbdaly@gmail.com>
2016-07-24 18:01:58 +0100

    Removed redundant code

--- test/components/Board.js
+++ test/components/Board.js
@@ -1,11 +1,10 @@
 import TestUtils from 'react-addons-test-utils';
 import React from 'react';
 import {findDOMNode} from 'react-dom';
-import {List, Map} from 'immutable';
 import Board from '../../js/components/Board';
 import {expect} from 'chai';
 
-const {renderIntoDocument, scryRenderedDOMComponentsWithClass, Simulate} = TestUtils;
+const {renderIntoDocument} = TestUtils;
 
 describe('Board', () => {
   it('renders the board', () => {
